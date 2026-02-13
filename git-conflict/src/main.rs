196cfa0a08820dd69e3f06a549444a486ab8885c
1nuc <ahmedalyafai85@gmail.com>
2026-02-14 04:27:06 +0800

    Save updates

--- git-conflict/src/main.rs
+++ git-conflict/src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+// use git2::RevertOptions
+use std::fs::File;
+use std::io::Write;
+fn main(){
+    let mut file=File::create("test.txt").unwrap();
+
+    file.write_all(b"Hello, world!");
 }
