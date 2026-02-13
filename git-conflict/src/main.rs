1958ded9c4b596dfdb43e31727f3014be0901442
1nuc <ahmedalyafai85@gmail.com>
2026-02-14 05:29:39 +0800

    Save notes

--- git-conflict/src/main.rs
+++ git-conflict/src/main.rs
@@ -1,11 +1,25 @@
 use git2::{Error, Repository};
 use std::fs::File;
 use std::io::Write;
+use std::path::Path;
+use std::env;
 fn main(){
-    let path= "../../../graphs-rust/graphs-lib/benches/my_bench.rs";
-    match Repository::discover(path){
-        Ok(repo) => println!("Repo found in this directory: {:?}", repo.workdir()),
-        Error=>println!("path is not found"),
-    }
+    let path=Path::new("../../../../ClearSet/FolioBridge_Source_Code/folio-bridge-ui/src/app/components/actionitemchart/actionitemchart.component.css");
+    return_path(path);
+    let dir=env::current_dir();
+    println!("dir: {:?}", dir);
+}
 
+
+#[allow(non_snake_case, unused_variables)]
+fn return_path(file_path: &Path){
+   match Repository::discover(file_path){
+       Ok(repo) => {
+           if let Some(path)=repo.workdir(){
+               let path_: &Path= path;
+               println!("Working repository: {:?}", path_);
+           }
+       }
+       Error => println!("Unable to find the repository path"),
+   }
 }
