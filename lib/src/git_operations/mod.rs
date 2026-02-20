cf8b27ffc97fd680ac9c36e7c9fb8b6a00a181a1
1nuc <ahmedalyafai85@gmail.com>
2026-02-21 04:28:35 +0800

    add lib function

--- lib/src/git_operations/mod.rs
+++ lib/src/git_operations/mod.rs
@@ -11,3 +11,109 @@ impl Branches{
        }
    } 
 }
+
+//TODO: staging changes
+
+fn staging(index: &mut Index, files: Vec<String>){
+    files.iter().map(|x| {
+        let path=Path::new(x);
+        index.add_path(path).expect("Error adding the file to the staging area");
+        }).collect::<Vec<_>>();
+}
+//TODO: Making a commit
+
+fn commit(mut index: Index, repo: Arc<Repository>)-> bool{
+    let _=index.write();
+    let tree=repo.find_tree(index.write_tree().unwrap()).unwrap();
+    let signature=repo.signature().unwrap().to_owned();
+    let message="merge conflict";
+    // get the heads commits
+    let head=repo.head().unwrap();
+    let parents_commits=&[&head.peel_to_commit().unwrap()];
+
+    match repo.commit(Some("HEAD"), &signature, &signature, message, &tree, parents_commits){
+        Ok(_val) => true,
+        Error => false,
+    }
+}
+//TODO: return the file with conditions  
+
+fn return_files(condition: Status, repo: Arc<Repository>)-> Option<Vec<String>>{
+    let mut options=StatusOptions::new();
+    options.include_untracked(false).recurse_untracked_dirs(false);
+    let status=repo.statuses(Some(&mut options)).unwrap();
+    let mut list_of_conflicted_files=Vec::new();
+    for i in status.iter(){
+        if i.status().contains(condition) && let Some(path)=i.path(){
+            list_of_conflicted_files.push(path.to_owned());
+        }
+    }
+    Some(list_of_conflicted_files)
+}
+//TODO: Merge function
+
+fn merge(repo: Repository,branch_1_commit: Commit, branch_2_commit: Commit) -> Result<Index, Error>{
+    let merge_options=MergeOptions::new();
+    match repo.merge_commits(&branch_1_commit,&branch_2_commit,Some(&merge_options)) {
+        Ok(index) =>{
+            Ok(index)
+        },
+        Error => {
+            Error
+        },
+    }
+}
+//TODO: Returning the directory path
+
+fn return_path(file_path: &Path) -> Option<Repository, > {
+   match Repository::discover(file_path){
+       Ok(repo) => {
+           if let Some(path)=repo.workdir(){
+               let path_: &Path= path;
+               Some(repo)
+           }
+           else{
+               println!("no path found for this repo");
+               None
+           }
+       }
+       Error => {
+           println!("Unable to find the repository path");
+           None
+       }
+   }
+}
+//TODO: a function to show merge options
+
+fn testing_conflict_detection(){
+    let args: Vec<String>=env::args().collect();
+    // let branch_1=args[1].clone();
+    // let branch_2=args[2].clone();
+    let dir=env::current_dir().unwrap();
+    if let Some(repo)=return_path(dir.as_path()){
+        let mut index=repo.index().unwrap();
+        if index.has_conflicts(){
+            let repository=Arc::new(repo);
+            let mut builder=CheckoutBuilder::new();
+            let checkout_builder=builder.use_ours(true);//specify the checkout build options to use
+                                                        //the ours (head) reference for the version
+                                                        //control switching
+            let repo=Arc::clone(&repository);
+            let files=return_files(Status::CONFLICTED, repository).unwrap();
+            // specify the files for which the checkout is to be held for
+            files.iter().map(|x| {
+                let _=checkout_builder.path(x).force();
+            }).collect::<Vec<_>>();
+            let _=repo.checkout_index(Some(&mut index), Some(checkout_builder));//revert back the
+                                                                                //current index to
+                                                                                //the index built
+                                                                                //from (head)
+            staging(&mut index, files); //stage the changes
+            match commit(index, repo){//commit the changes
+                true => println!("conflict is resolved"),
+                false => println!("error resolving the conflict"),
+            }
+            // resolve_conflicts(index, repo);
+        }
+    }
+}
