240a44f57e5621ae45b31e0b1e00ef8f2bd8547a
1nuc <ahmedalyafai85@gmail.com>
2026-06-27 06:55:57 +0800

    update contents

--- crates/git-conflict/src/lib.rs
+++ crates/git-conflict/src/lib.rs
@@ -4,7 +4,7 @@ use git2::{Commit, Error, Index, Repository, Status};
 pub mod git_operations;
 
 pub trait GitOps <'a>{
-    fn display_commits(&self);
+    fn display_commits(&mut self);
     fn staging(&mut self, files: Vec<String>);
     fn commit(&mut self)-> bool;
     fn return_files(&self,condition: Status)-> Option<Vec<String>>;
