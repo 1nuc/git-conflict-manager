27e0e0248de1385e9f6c984e4cdc54086a02f494
1nuc <ahmedalyafai85@gmail.com>
2026-07-05 09:55:10 +0800

    save notes

---
+++ crates/git-conflict/src/merge_trees.rs
@@ -0,0 +1,20 @@
+use crate::{Actions, git_src::Repo};
+
+struct TreeVersion<'a>{
+    tr: Repo<'a>,
+}
+
+
+impl<'a> Actions for TreeVersion<'a>{
+    fn index(&self) -> std::cell::RefMut<git2::Index> {
+        self.tr.index.0.borrow_mut()
+    }
+
+    fn repo(&self) -> std::cell::RefMut<git2::Repository> {
+        self.tr.repo.0.borrow_mut()
+    }
+
+    fn branches(&self) -> crate::git_src::Branches {
+        self.tr.branches.clone()
+    }
+}
