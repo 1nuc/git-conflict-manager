fd7c7a816478ab4a3e81155e67e60a406c961366
1nuc <ahmedalyafai85@gmail.com>
2026-07-20 01:56:05 +0800

    store descriptions

--- crates/git-conflict-cli/src/desc.rs
+++ crates/git-conflict-cli/src/desc.rs
@@ -55,20 +55,20 @@ impl<'a> Description<'a> {
 
     pub fn combination_decs(&self) ->Line<'a> {
         Line::from(vec![
-            "Merge both Head and foreign branch's changes".white(),
-            "The conflict is resolved through accepting both branches' changes".white(),
-            "Both branches changes will be accepted and there won't be any abondoned changes".white(),
-            "The new merge commit will contain both branches' commits as the parents for the new commit, the branch you are currently at will be the ancestor".white(),
+            Span::styled("Merge both Head and foreign branch's changes", Style::new().white()),
+            Span::styled("The conflict is resolved through accepting both branches' changes", Style::new().white()),
+            Span::styled("Both branches changes will be accepted and there won't be any abondoned changes", Style::new().white()),
+            Span::styled("The new merge commit will contain both branches' commits as the parents for the new commit, the branch you are currently at will be the ancestor", Style::new().white()),
         ])
     }
 
     pub fn tree_desc(&self) -> Line<'a>{
         Line::from(vec![
-            "Resolve conflict through tree index-based merging".white(),
-            "If parent interference is enabled: The tree of the head branch or the foreign branch will be merged along side that parent tree".white(),
-            "If parent interference is not enabled: The git index tree will be merged with either (Head, Foreign) Trees".white(),
-            "Use this method when you want to have better control over your branches and avoid losing parent changes".white(),
-            "The method is prefered to keep teams on track and avoid losing sync that is caused due to extremely divergent changes".white(),
+            Span::styled("Resolve conflict through tree index-based merging", Style::new().white()),
+            Span::styled("If parent interference is enabled: The tree of the head branch or the foreign branch will be merged along side that parent tree", Style::new().white()),
+            Span::styled("If parent interference is not enabled: The git index tree will be merged with either (Head, Foreign) Trees", Style::new().white()),
+            Span::styled("Use this method when you want to have better control over your branches and avoid losing parent changes", Style::new().white()),
+            Span::styled("The method is prefered to keep teams on track and avoid losing sync that is caused due to extremely divergent changes", Style::new().white()),
         ])
     }
 }
